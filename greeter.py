880cf7b9c3737226fba3cd45ab2310506ad14716
Norberto Lara <norblara@gmail.com>
2019-05-29 22:55:53 -0500

    ch 8 functiions

--- greeter.py
+++ greeter.py
@@ -1,5 +1,11 @@
-prompt = "If you tell us who you are, we can personalize the messages you see."
-prompt += "\nWhat is you first name? "
+#prompt = "If you tell us who you are, we can personalize the messages you see."
+#prompt += "\nWhat is you first name? "
 
-name = input(prompt)
-print("Hello, " + name + '!')
+#name = input(prompt)
+#print("Hello, " + name + '!')
+
+def greet_user(username):
+	"""Display a simple greeting"""
+	print("Hello, " + username.title() + "!")
+
+greet_user("jesse")
