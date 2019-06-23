0468f7237e171e97dce6e6c292a46c43d1c8af2b
Norberto Lara <norblara@gmail.com>
2019-06-22 22:57:26 -0500

    exercise 9.4

--- restaurant.py
+++ restaurant.py
@@ -1,8 +1,9 @@
 class Restaurant():
 	"""Model for a generic restaurant"""
-	def __init__(self, name, cusine):
+	def __init__(self, name, cusine, number):
 		self.name = name
 		self.cusine = cusine
+		self.number_served = 0
 
 	def describe_restaurant(self):
 		print(self.name + " is a " + self.cusine + " restaurant.")
@@ -10,11 +11,22 @@ class Restaurant():
 	def open_restaurant(self):
 		print(self.name + " is now open")
 
-first = Restaurant('chillis', 'texmex')
-second = Restaurant('carinos', 'itallian')
+	def set_number_served(self, served):
+		self.number_served += served
+
+first = Restaurant('chillis', 'texmex',0)
+second = Restaurant('carinos', 'itallian',0)
+third = Restaurant('deli', 'sandwiches', 0)
 
 first.describe_restaurant()
 first.open_restaurant()
 
 second.describe_restaurant()
 second.open_restaurant()
+
+print(third.number_served)
+third.describe_restaurant()
+third.set_number_served(3)
+print(third.number_served)
+
+
