8a5d194de5417809a7b0eaf78bd899f80ebf3110
Norberto Lara <norblara@gmail.com>
2019-07-04 06:35:22 -0500

    Overriding methods from parent class

--- electric_car.py
+++ electric_car.py
@@ -35,11 +35,23 @@ class Car():
 class ElectricCar(Car):
 	"""Represents aspects of a car, specific to electric vehicles."""
 	def __init__(self, make, model, year):
-		"""Initialize attributes of the jparent clas."""
+		"""Initialize attributes of the jparent clas.
+		Then initialize attributes specific to an electric car."""
+
 		super().__init__(make, model, year)
+		self.battery_size = 70
+
+	def describe_battery(self):
+		"""Ptint a statement describing the battery size."""
+		print("This car has a " + str(self.battery_size) + "-KWh battery.")
+
+	def fill_gas_tank():
+		"""Electric cars don't have gas tanks."""
+		print("This car doesn't need a gas tank!")
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
