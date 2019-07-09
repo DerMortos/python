2ea2113217b4374b7f045c7ba7539a2d80a20682
Norberto Lara <norblara@gmail.com>
2019-07-09 11:50:29 -0500

    Ordered Dictionaries

--- favorite_languages.py
+++ favorite_languages.py
@@ -1,10 +1,12 @@
-favorite_languages = {
-	'jen': 'python',
-	'sarah': 'c',
-	'edward': 'ruby',
-	'phil': 'python',
-}
+from collections import OrderedDict
 
-print('The following languagews have been mentioned: ')
-for language in set(favorite_languages.values()):
-	print(language.title())
+favorite_languages = OrderedDict()
+
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
+
+for name, language in favorite_languages.items():
+	print(name.title() + "'s favorite language is " +
+		language.title() + ".")
