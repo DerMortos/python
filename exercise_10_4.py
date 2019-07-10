d3f187a48a33f10ff2e454069ae38dc0b9c25329
Norberto Lara <norblara@gmail.com>
2019-07-10 12:46:21 -0500

    exercise 10.4 completed

--- exercise_10_4.py
+++ exercise_10_4.py
@@ -2,11 +2,10 @@ filename = "text_files/guest_book.txt"
 prompt = "Please enter your name: "
 
 with open(filename, 'a') as file_object:
-	guests = True
-	while guests:
+	while True:
 		message = input(prompt)
 
 		if message == 'quit':
-			active = False
+			break
 		else:
 			file_object.write(message + '\n')
